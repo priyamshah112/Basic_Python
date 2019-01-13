8e07abfb887c21155289cea80b0f6d6bd9c7b233
Priyam Shah <priyamshah112@gmail.com>
2019-01-13 19:12:47 +0530

    Interfacing Odroid XU4 and Arduino Mega

--- Arduino/SerialTest.py
+++ Arduino/SerialTest.py
@@ -2,4 +2,6 @@ from time import sleep
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
 ser.write('t'.encode()) # Convert the decimal number to ASCII then send it to the Arduino
-
+while True:
+	for line in ser.read():
+		print(chr(line))
